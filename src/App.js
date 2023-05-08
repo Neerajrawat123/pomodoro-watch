d6a5aede1e7bbc1a1445566b102fdf0aaf543130
neeraj <neerajrawat78200@gmail.com>
2023-05-08 21:09:45 +0530

    good
    git status
    
    i hghh
    .git/COMMIT_EDITPlease enter the commit message for your changes. Lines starting
    .git/COMMIT_EDITith '#' will be ignored, and an empty message aborts the commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from "react";
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(params) {
+  const [count, setcount] = useState(1500)
+  const [isCountdown, setisCountdown] = useState(false)
+  // const [break, setbreak] = useState(0)
+  useEffect(() => {
+    let interval
+    if (count && isCountdown>0) {
+       interval = setInterval(() => {
+        setcount((prevcount)=>prevcount-1)
+        
+      }, 1000);
+      
+    }else if(count===0){
+      clearInterval(interval)
+      setisCountdown(false)
+      alert("time is over")
+
+    }
+    
+  
+    return () => {clearInterval(interval)
+      
+    }
+  }, [count,isCountdown])
+  const handleStart=()=>{
+    setisCountdown(true)
+    
+
+  }
+  const handlePomodoro = ()=>{
+    setisCountdown(false)
+    setcount(1500)
+
+  }
+  const handleLong = () =>{
+    setisCountdown(false)
+
+    setcount(900)
+
+  }
+  const handleShort = ()=>{
+    setisCountdown(false)
+
+    setcount(300)
+
+  }
+
+  const handlePause = ()=>{
+    setisCountdown(false)
+  }
+  const formatCountdownTime = new Date(count * 1000).toISOString().substr(11, 8);
+
+  
+  
+  return(
+    <div className="app">
+      <div className="container">
+        <div className="btn-container">
+
+        <button onClick={handlePomodoro}>Pomodoro</button>
+        <button onClick={handleShort}>short break</button>
+        <button onClick={handleLong}>long break</button>
+        </div>
+
+        <div className="timer">{formatCountdownTime}</div>
+        {isCountdown?
+        (<button className="pause main" onClick={handlePause}>pause </button>):
+        ( <button className="start main" onClick={handleStart}>start</button>)
+        
+
+        }
+
+      </div>
     </div>
-  );
-}
 
+  )
+  
+}
 export default App;
+
